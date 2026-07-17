fe7eee68d4210e4bab319d8284198287b6f755a8
sumit  sahu <sumitsahu1684@gmail.com>
2026-07-17 19:53:58 +0530

    Time: 39 ms (78.67%), Space: 134.5 MB (25.33%) - LeetHub

--- 3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.cpp
+++ 3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.cpp
@@ -2,28 +2,28 @@ class Solution {
 public:
     vector<int> gcdValues(vector<int>& nums, vector<long long>& queries) {
 
-        int mx = *max_element(nums.begin(), nums.end());
-        vector<int> freq(mx+1);
+        int mx = *max_element(nums.begin(),nums.end());
+        vector<int> frequency(mx+1);
         for(int x : nums){
-            freq[x]++;
+            frequency[x]++;
         }
         vector<long long> divisible(mx+1);
         for (int i=1;i<=mx;i++){
             for (int j=i;j<=mx;j+=i)
-                divisible[i] +=freq[j];
+                divisible[i] +=frequency[j];
         }
-        vector<long long> exact(mx+1);
+        vector<long long> arr(mx+1);
         for (int i=mx;i>=1;i--){
-            exact[i] = divisible[i]*(divisible[i]-1)/2;
+            arr[i] = divisible[i]*(divisible[i]-1)/2;
             for (int j=2*i; j<=mx;j+=i)
-                exact[i]-=exact[j];
+                arr[i]-=arr[j];
         }
-        vector<long long> pref(mx+1);
+        vector<long long> brr(mx+1);
         for (int d=1;d<=mx;d++)
-            pref[d] = pref[d-1]+exact[d];
+            brr[d] = brr[d-1]+arr[d];
         vector<int> ans;
-        for (long long q:queries){
-            ans.push_back(lower_bound(pref.begin(),pref.end(),q + 1)-pref.begin());
+        for(long long q:queries){
+            ans.push_back(lower_bound(brr.begin(),brr.end(),q + 1)-brr.begin());
         }
         return ans;
     }
